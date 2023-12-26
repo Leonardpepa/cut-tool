f82067af00b70887aaf843dc19abf5a48eb5c0e3
Leonardpepa <leopepp4@gmail.com>
2023-12-26 03:37:24 +0200

    testing things

--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, cut tool")
+
+	//filename := "tests/sample.tsv"
+	filename := "tests/sample.tsv"
+
+	scanner := getScanner(openFile(filename))
+
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		r := strings.NewReader(line)
+		b1 := make([]byte, 1)
+		b2 := make([]byte, 1)
+		b5 := make([]byte, 1)
+
+		_, err := r.ReadAt(b1, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = r.ReadAt(b2, 1)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = r.ReadAt(b5, 4)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%s%s%s\n", string(b1), string(b2), string(b5))
+	}
+}
+
+func getScanner(file *os.File) *bufio.Scanner {
+	return bufio.NewScanner(file)
+}
+func openFile(filename string) *os.File {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return file
 }
