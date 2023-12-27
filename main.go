3a96984f14056f8325a1c91dfd1fe7ab2a7609af
Leonardpepa <leopepp4@gmail.com>
2023-12-27 20:57:33 +0200

    intial try to parse the list arguments

--- main.go
+++ main.go
@@ -1,58 +1,94 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
 	"log"
-	"os"
+	"strconv"
 	"strings"
 )
 
+type List struct {
+	// set of numbers
+	numbers map[int]int
+}
+
+func (list *List) appendNumber(from int, to int) {
+	list.numbers[from] = to
+}
+
+func (list *List) appendListOfNumber(nums []int) {
+	for _, num := range nums {
+		list.appendNumber(num, num)
+	}
+}
+
+func parseList(data string) List {
+	list := List{
+		numbers: make(map[int]int),
+	}
+
+	values := strings.Split(data, ",")
+
+	for _, val := range values {
+
+		isRange := strings.Contains(val, "-")
+
+		if isRange {
+			start, end := parseRange(val)
+			list.appendNumber(start, end)
+			continue
+		}
+
+		num, err := strconv.Atoi(val)
+
+		if err == nil {
+			list.appendNumber(num, num)
+		}
+	}
+
+	return list
+}
+
+func parseRange(data string) (start, end int) {
+
+	values := strings.Split(data, "-")
+
+	if len(values) != 2 {
+		log.Fatal("error more tha tow numbers in range")
+	}
+
+	start = parseEmptyNumber(values[0], 1)
+	end = parseEmptyNumber(values[1], -1)
+
+	if end != -1 && start > end {
+		log.Fatal("invalid decreasing range")
+	}
+
+	return
+}
+
+func parseEmptyNumber(value string, defaultValue int) int {
+	if value != "" {
+		num, err := strconv.Atoi(value)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return num
+	}
+	return defaultValue
+}
+
 func main() {
 
-	//filename := "tests/sample.tsv"
-	filename := "tests/sample.tsv"
+	f := flag.String("f", "", "fields_list")
 
-	scanner := getScanner(openFile(filename))
+	flag.Parse()
 
-	scanner.Split(bufio.ScanLines)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		r := strings.NewReader(line)
-		b1 := make([]byte, 1)
-		b2 := make([]byte, 1)
-		b5 := make([]byte, 1)
-
-		_, err := r.ReadAt(b1, 0)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		_, err = r.ReadAt(b2, 1)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		_, err = r.ReadAt(b5, 4)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%s%s%s\n", string(b1), string(b2), string(b5))
-	}
-}
-
-func getScanner(file *os.File) *bufio.Scanner {
-	return bufio.NewScanner(file)
-}
-func openFile(filename string) *os.File {
-	file, err := os.Open(filename)
-
-	if err != nil {
-		log.Fatalln(err)
+	if *f == "" {
+		log.Fatal("no f provided")
 	}
 
-	return file
+	fmt.Println(parseList(*f))
 }
