fd770a4a3194bea3eef4dc0500c246854f3ebfb4
Leonardpepa <leopepp4@gmail.com>
2024-01-02 02:13:21 +0200

    update usage

--- main.go
+++ main.go
@@ -234,5 +234,7 @@ Each range is one of:
   N     N'th byte, character or field, counted from 1
   N-    from N'th byte, character or field, to end of line
   N-M   from N'th to M'th (included) byte, character or field
-  -M    from first to M'th (included) byte, character or field`, filepath.Base(os.Args[0]))
+  -M    from first to M'th (included) byte, character or field
+Original cut tool for linux is here: Full documentation <https://www.gnu.org/software/coreutils/cut>
+`, filepath.Base(os.Args[0]))
 }
